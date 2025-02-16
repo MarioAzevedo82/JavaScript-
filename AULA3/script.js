6b13448287aa858f3174172feecbe41bcaf8ce76
Mario Azevedo <mariobct@msn.com>
2025-02-16 09:07:28 -0300

    script condicionais

---
+++ AULA3/script.js
@@ -0,0 +1,26 @@
+
+function adicionarTarefa(){
+   
+    
+    const inputTarefa = document.getElementById("inputTarefa")
+    let tarefa = inputTarefa.value.trim()
+    
+    const msn = document.getElementById("mensagem")
+    
+    if (tarefa == "") {
+        let mensagemErro = "Digite uma mensagem para adicioná-la!"
+        msn.textContent = mensagemErro
+    } else {  
+        let mensagem = "Tarefa adicionada com sucesso!";
+        msn.textContent = mensagem;
+
+        const listaTarefas = document.getElementById("listaTarefas")
+        let novaTarefa = document.createElement("li")
+        novaTarefa.textContent = tarefa
+        listaTarefas.appendChild(novaTarefa)
+
+    }   
+    
+    inputTarefa.value = ""
+
+}
