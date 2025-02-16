b804e58a4d3c48f9737fcfe6dd55ac2590edeb80
Mario Azevedo <mariobct@msn.com>
2025-02-16 09:06:34 -0300

    atualizações

--- AULA2/script.js
+++ AULA2/script.js
@@ -1,20 +1,20 @@
-
 function adicionarTarefa(){
-    let mensagem = "Tarefa adicionada com sucesso!";
+    let mensagem = "Tarefa adicionada com sucesso!"//1, 9 fianl descomenta 
     
-    let inputTarefa = document.getElementById("inputTarefa")
-    let tarefa = inputTarefa.value
-    document.getElementById("mensagem").textContent = mensagem;
+    
+    let inputTarefa = document.getElementById("inputTarefa") //2
+    let tarefa = inputTarefa.value //2
+    document.getElementById("mensagem").textContent = mensagem //1, 3 qndo colocou tarefa ao invés de mensagem
+    
+    let listaTarefas = document.getElementById("listaTarefas") // 5 criando a lista
+    let novaTarefa = document.createElement("li") // 6 criando os elementos da lista li
 
-    let listaTarefas = document.getElementById("listaTarefas")
-    let novaTarefa = document.createElement("li")
+    novaTarefa.textContent = tarefa // 8 o valor digitado vai pra dentro da li
 
-    novaTarefa.textContent = tarefa
+    listaTarefas.appendChild(novaTarefa) // 7 colocou o li dentro do ul
+    //console.log(listaTarefas)
 
-    listaTarefas.appendChild(novaTarefa)
-
-
-    inputTarefa.value = ""
+    inputTarefa.value = "" // 4 limpar o campo automaticamente
 }
 
 
