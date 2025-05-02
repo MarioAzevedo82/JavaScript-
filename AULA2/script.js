14934fd82cd9733d9d3e2c8095e659cba317df3a
Mario Azevedo <mariobct@msn.com>
2025-05-02 07:17:41 -0300

    criado o li e append

--- AULA2/script.js
+++ AULA2/script.js
@@ -4,6 +4,13 @@ function adicionarTarefa(){
     let inputTarefa = document.getElementById("inputTarefa") //2 pegando a tarefa 
     let tarefa = inputTarefa.value // 3 pegando o valor do q foi digitado
     document.getElementById('mensagem').textContent = tarefa //4 expondo a tarefa
+
+    let listaTarefas = document.getElementById("listaTarefas") // 6 criada a lista
+    let novaTarefa = document.createElement("li")
+
+    novaTarefa.textContent = tarefa
+
+    listaTarefas.appendChild(novaTarefa)
     
     inputTarefa.value = "" // 5 zerar o campo input
   }
