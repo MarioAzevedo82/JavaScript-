7ab098d76d01610ac80eed40e94f9f109c274be4
Mario Azevedo <mariobct@msn.com>
2025-05-02 06:46:29 -0300

    expondo a tarefa digitada e zerando o input

--- AULA2/script.js
+++ AULA2/script.js
@@ -1,7 +1,9 @@
 function adicionarTarefa(){
-    let mensagem = "Tarefa adicionada com sucesso!"
-    document.getElementById('mensagem').textContent = mensagem //1 mensagem de sucesso
-
+    //let mensagem = "Tarefa adicionada com sucesso!"
+    
     let inputTarefa = document.getElementById("inputTarefa") //2 pegando a tarefa 
     let tarefa = inputTarefa.value // 3 pegando o valor do q foi digitado
+    document.getElementById('mensagem').textContent = tarefa //4 expondo a tarefa
+
+    inputTarefa.value = "" // 5 zerar o campo input
   }
