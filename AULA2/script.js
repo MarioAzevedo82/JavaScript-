7338299d146d0883b3ff89a1e3fe80f4f8ba9acc
Mario Azevedo <mariobct@msn.com>
2025-05-02 07:20:40 -0300

    volta msg de sucesso

--- AULA2/script.js
+++ AULA2/script.js
@@ -1,16 +1,16 @@
 function adicionarTarefa(){
-    //let mensagem = "Tarefa adicionada com sucesso!"
+    let mensagem = "Tarefa adicionada com sucesso!"
     
     let inputTarefa = document.getElementById("inputTarefa") //2 pegando a tarefa 
     let tarefa = inputTarefa.value // 3 pegando o valor do q foi digitado
-    document.getElementById('mensagem').textContent = tarefa //4 expondo a tarefa
+    document.getElementById('mensagem').textContent = mensagem //4 expondo a tarefa //10 volta a mensagem de sucesso
 
     let listaTarefas = document.getElementById("listaTarefas") // 6 criada a lista
-    let novaTarefa = document.createElement("li")
+    let novaTarefa = document.createElement("li") // 7 cria o li
 
-    novaTarefa.textContent = tarefa
+    novaTarefa.textContent = tarefa // 8 colocando no li o conteudo de texto da tarefa
 
-    listaTarefas.appendChild(novaTarefa)
+    listaTarefas.appendChild(novaTarefa) // 9 adiciona o elemento filho (li com o valor de tarefa)
     
     inputTarefa.value = "" // 5 zerar o campo input
   }
