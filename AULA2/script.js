ef188e889657b795f19abc4430ebc0a6ebee4275
Mario Azevedo <mariobct@msn.com>
2025-05-02 06:51:53 -0300

    criado o ul listaTarefas

--- AULA2/script.js
+++ AULA2/script.js
@@ -4,6 +4,6 @@ function adicionarTarefa(){
     let inputTarefa = document.getElementById("inputTarefa") //2 pegando a tarefa 
     let tarefa = inputTarefa.value // 3 pegando o valor do q foi digitado
     document.getElementById('mensagem').textContent = tarefa //4 expondo a tarefa
-
+    
     inputTarefa.value = "" // 5 zerar o campo input
   }
