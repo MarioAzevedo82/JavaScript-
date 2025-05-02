de7c8e8ed469aec8e7420d776ce2e994743186d5
Mario Azevedo <mariobct@msn.com>
2025-05-02 06:39:46 -0300

    let tarefa

--- AULA2/script.js
+++ AULA2/script.js
@@ -1,4 +1,7 @@
 function adicionarTarefa(){
     let mensagem = "Tarefa adicionada com sucesso!"
-    document.getElementById('mensagem').textContent = mensagem
+    document.getElementById('mensagem').textContent = mensagem //1 mensagem de sucesso
+
+    let inputTarefa = document.getElementById("inputTarefa") //2 pegando a tarefa 
+    let tarefa = inputTarefa.value // 3 pegando o valor do q foi digitado
   }
