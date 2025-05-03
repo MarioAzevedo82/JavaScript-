1a9f18ad4bbf7708ffd2e7f1989d7f0152689c21
Mario Azevedo <mariobct@msn.com>
2025-05-03 13:39:58 -0300

    JS inicial para aula 3

--- AULA3/script.js
+++ AULA3/script.js
@@ -1,27 +1,16 @@
 
 function adicionarTarefa(){
-   
+    let mensagem = "Tarefa adicionada com sucesso!"
     
-    const inputTarefa = document.getElementById("inputTarefa")
-    let tarefa = inputTarefa.value.trim()
-    
-    const msn = document.getElementById("mensagem")
-    
-    if (tarefa == "") {
-        let mensagemErro = "Digite uma mensagem para adicioná-la!"
-        msn.textContent = mensagemErro
-        
-    } else {  
-        let mensagem = "Tarefa adicionada com sucesso!";
-        msn.textContent = mensagem;
+    let inputTarefa = document.getElementById("inputTarefa") 
+    let tarefa = inputTarefa.value 
+    document.getElementById('mensagem').textContent = mensagem 
+    let listaTarefas = document.getElementById("listaTarefas") 
+    let novaTarefa = document.createElement("li") 
 
-        const listaTarefas = document.getElementById("listaTarefas")
-        let novaTarefa = document.createElement("li")
-        novaTarefa.textContent = tarefa
-        listaTarefas.appendChild(novaTarefa)
+    novaTarefa.textContent = tarefa 
 
-    }   
+    listaTarefas.appendChild(novaTarefa) 
     
-    inputTarefa.value = ""
-
-}
+    inputTarefa.value = "" 
+  }
