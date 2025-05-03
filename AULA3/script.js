840131dfd448a5d47845c9710a755527ba0c1d10
Mario Azevedo <mariobct@msn.com>
2025-05-03 14:52:10 -0300

    criadas const

--- AULA3/script.js
+++ AULA3/script.js
@@ -1,9 +1,9 @@
 
 function adicionarTarefa(){
     
-    let inputTarefa = document.getElementById("inputTarefa") 
+    const inputTarefa = document.getElementById("inputTarefa") 
     let tarefa = inputTarefa.value.trim() //remove os espaços em branco no início e no fim do código(trim = aparar)
-    let mensagem = document.getElementById('mensagem')
+    const mensagem = document.getElementById('mensagem')
     
     if(tarefa == "") {
         let mensagemErro = "Digite uma tarefa para adicioná-la!"
@@ -12,7 +12,7 @@ function adicionarTarefa(){
         let mensagemSucesso = "Tarefa adicionada com sucesso!"
         mensagem.textContent = mensagemSucesso
 
-        let listaTarefas = document.getElementById("listaTarefas") 
+        const listaTarefas = document.getElementById("listaTarefas") 
         let novaTarefa = document.createElement("li") 
         novaTarefa.textContent = tarefa 
         listaTarefas.appendChild(novaTarefa) 
