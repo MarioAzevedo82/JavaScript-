af243494e3d003f332274e6a381f799aaa9c9f7c
Mario Azevedo <mariobct@msn.com>
2025-05-03 14:43:48 -0300

    adicionando a variável mensagem

--- AULA3/script.js
+++ AULA3/script.js
@@ -3,13 +3,15 @@ function adicionarTarefa(){
     
     let inputTarefa = document.getElementById("inputTarefa") 
     let tarefa = inputTarefa.value.trim() //remove os espaços em branco no início e no fim do código(trim = aparar)
+    let mensagem = document.getElementById('mensagem')
     
     if(tarefa == "") {
         let mensagemErro = "Digite uma tarefa para adicioná-la!"
-        document.getElementById('mensagem').textContent = mensagemErro
+        mensagem.textContent = mensagemErro
     } else {
         let mensagemSucesso = "Tarefa adicionada com sucesso!"
-        document.getElementById('mensagem').textContent = mensagemSucesso
+        mensagem.textContent = mensagemSucesso
+
         let listaTarefas = document.getElementById("listaTarefas") 
         let novaTarefa = document.createElement("li") 
         novaTarefa.textContent = tarefa 
