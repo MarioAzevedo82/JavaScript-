ade55566754de692bd0a9be2e5e8a55bf16d373e
Mario Azevedo <mariobct@msn.com>
2025-05-03 14:29:51 -0300

    criados if/else

--- AULA3/script.js
+++ AULA3/script.js
@@ -1,10 +1,17 @@
 
 function adicionarTarefa(){
-    let mensagem = "Tarefa adicionada com sucesso!"
     
     let inputTarefa = document.getElementById("inputTarefa") 
     let tarefa = inputTarefa.value 
-    document.getElementById('mensagem').textContent = mensagem 
+    
+    if(tarefa == "") {
+        let mensagemErro = "Digite uma tarefa para adicioná-la!"
+        document.getElementById('mensagem').textContent = mensagemErro
+    } else {
+        let mensagemSucesso = "Tarefa adicionada com sucesso!"
+        document.getElementById('mensagem').textContent = mensagemSucesso
+    }
+
     let listaTarefas = document.getElementById("listaTarefas") 
     let novaTarefa = document.createElement("li") 
 
@@ -13,4 +20,7 @@ function adicionarTarefa(){
     listaTarefas.appendChild(novaTarefa) 
     
     inputTarefa.value = "" 
+
+
+
   }
