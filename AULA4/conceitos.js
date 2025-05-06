b91ed9e707333a080fea0e684134b631dc62da0e
Mario Azevedo <mariobct@msn.com>
2025-05-06 07:39:02 -0300

    array indexOf

---
+++ AULA4/conceitos.js
@@ -0,0 +1,11 @@
+// let fruta = "maca"
+// alert(fruta)
+// fruta = "banana"
+// alert(fruta)
+
+let frutas = ["maca", "banana", "mamao"]
+//alert(frutas)
+
+alert(frutas.indexOf("banana"))
+
+alert(frutas[0])
